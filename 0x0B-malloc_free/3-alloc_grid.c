e90cfc9f7871f4dde6037557eaa7e4a7516b67ed
ace07056506206 <ace07056506206@gmail.com>
2023-04-11 06:34:29 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,37 @@
 
 int **alloc_grid(int width, int height)
 
+{
+
+	int **mee;
+
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+
+		return (NULL);
+	mee = malloc(sizeof(int *) * height);
+	if (mee == NULL)
+
+		return (NULL);
+	for (x = 0; x < height; x++)
 	{
-		int **mee;
+		mee[x] = malloc(sizeof(int) * width);
+		if (mee[x] == NULL)
 
-		int x, y;
-
-		if (width <= 0 || height <= 0)
-		return (NULL);
-		mee = malloc(sizeof(int *) * height);
-		if (mee == NULL)
-		return (NULL);
-		for (x = 0; x < height; x++)
-			{
-				mee[x] = malloc(sizeof(int) * width);
-				if (mee[x] == NULL)
-				{
-					for (; x >= 0; x--)
-					free(mee[x]);
-					free(mee);
-					return (NULL);
-				}
-			}
-		for (x = 0; x < height; x++)
 		{
-			for (y = 0; y < width; y++)
-			mee[x][y] = 0;
+			for (; x >= 0; x--)
+				free(mee[x]);
+			free(mee);
+			return (NULL);
 		}
-		return (mee);
 	}
+
+	for (x = 0; x < height; x++)
+	{
+
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
+	return (mee);
+}
