44e37ae573672142e7a65bfb4819d828d4aa86fd
ace07056506206 <ace07056506206@gmail.com>
2023-04-11 06:15:23 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include "main.h"
 
 /**
-
  * _strdup - duplicate to new memory space location
-
  * @str: char
-
  * Return: 0
-
  */
 
 char *_strdup(char *str)
@@ -22,35 +16,16 @@ char *_strdup(char *str)
 
 	int i, r = 0;
 
-
-
 	if (str == NULL)
-
 		return (NULL);
-
 	i = 0;
-
 	while (str[i] != '\0')
-
 		i++;
-
-
-
 	aaa = malloc(sizeof(char) * (i + 1));
-
-
-
 	if (aaa == NULL)
-
 		return (NULL);
-
-
 	for (r = 0; str[r]; r++)
-
 		aaa[r] = str[r];
-
-
-
 	return (aaa);
 
 }
